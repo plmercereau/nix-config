07a86dc3238c7884673bcc5b1b7642ba8c0c98f0
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2024-02-18 14:03:48 +0100

    feat: home-manager, vpn and k8s

--- home-manager/wireguard.nix
+++ home-manager/wireguard.nix
@@ -7,35 +7,64 @@
   ...
 }:
 with lib; let
+  interface = "wg0";
   bastion = (findFirst (machine: machine.config.settings.networking.vpn.bastion.enable) (throw "bastion not found") (attrValues nixosConfigurations)).config;
-  inherit (bastion.settings.networking.vpn.bastion) cidr domain port;
+  inherit (bastion.settings.networking.vpn.bastion) port;
   inherit (config.home) username;
   inherit (bastion.settings.networking.vpn.bastion.extraPeers.${username}) id;
 
-  wgConfig = pkgs.writeText "wg0.conf" (generators.toINI {} {
+  hostNetwork = bastion.settings.networking.vpn.bastion;
+  k8sNetwork = bastion.settings.services.kubernetes.vpn;
+
+  baseConfig = {
     Interface = {
-      Address = bastion.lib.vpn.machineIp cidr id;
-      PostUp = pkgs.writeShellScript "postup.sh" ''
-        # Add the route to the VPN network
-        mkdir -p /etc/resolver
-        cat << EOF > /etc/resolver/vpn
-        port 53
-        domain ${domain}
-        search ${domain}
-        nameserver ${bastion.lib.vpn.ip}
-        EOF
-      '';
+      Address = [(bastion.lib.vpn.machineIp hostNetwork.cidr id) (bastion.lib.vpn.machineIp k8sNetwork.cidr id)];
+      # TODO if we want to make this home-manager module more generic, postup and postdown should work for other non-macOS systems
+      PostUp = let
+        # TODO for some reason, ${config.age.secrets.vpn.path} does not point to the right place when used in PostUp
+        privateKeyPath = "/run/agenix/vpn";
+      in
+        pkgs.writeShellScript "postup.sh" ''
+          ${pkgs.wireguard-tools}/bin/wg set $(${pkgs.wireguard-tools}/bin/wg show interfaces) private-key ${privateKeyPath}
+          # Add the route to the VPN network
+          mkdir -p /etc/resolver
+          cat << EOF > /etc/resolver/${hostNetwork.domain}
+          port 53
+          domain ${hostNetwork.domain}
+          search ${hostNetwork.domain}
+          nameserver ${bastion.lib.vpn.machineIp hostNetwork.cidr bastion.settings.networking.vpn.id}
+          EOF
+          cat << EOF > /etc/resolver/${k8sNetwork.domain}
+          port 53
+          domain ${k8sNetwork.domain}
+          search ${k8sNetwork.domain}
+          nameserver ${bastion.lib.vpn.machineIp k8sNetwork.cidr bastion.settings.networking.vpn.id}
+          EOF
+        '';
       PostDown = pkgs.writeShellScript "postdown.sh" ''
-        rm -f /etc/resolver/vpn
+        rm -f /etc/resolver/${hostNetwork.domain}
+        rm -f /etc/resolver/${k8sNetwork.domain}
       '';
     };
     Peer = {
       PublicKey = bastion.settings.networking.vpn.publicKey;
-      AllowedIPs = cidr;
+      AllowedIPs = [hostNetwork.cidr k8sNetwork.cidr];
       Endpoint = "${bastion.settings.networking.publicIP}:${toString port}";
       PersistentKeepalive = 25;
     };
-  });
+  };
+
+  customToINI = generators.toINI {
+    mkKeyValue = generators.mkKeyValueDefault {
+      mkValueString = v:
+        if isList v
+        then concatStringsSep "," v
+        else if isString v
+        then v
+        # and delegates all other values to the default generator
+        else generators.mkValueStringDefault {} v;
+    } " = ";
+  };
 in {
   imports = [agenix.homeManagerModules.default];
   age.identityPaths = ["${config.home.homeDirectory}/.ssh/id_ed25519"];
@@ -49,17 +78,16 @@ in {
     createConfig = pkgs.writeScript "create-config" ''
       ${deleteConfig}
       mkdir -p $(dirname ${path})
-      cp ${wgConfig} ${path}
-      chmod u+w ${path}
-      ${pkgs.crudini}/bin/crudini --set ${path} Interface PrivateKey $(${pkgs.coreutils}/bin/cat ${config.age.secrets.vpn.path})
+      ${pkgs.coreutils}/bin/install ${pkgs.writeText "wg0.conf" (customToINI baseConfig)} ${path}
     '';
     wgup = pkgs.writeScriptBin "wgup" ''
       ${createConfig}
-      sudo wg-quick up ${path}
+      sudo ${pkgs.wireguard-tools}/bin/wg-quick down ${path}
+      sudo ${pkgs.wireguard-tools}/bin/wg-quick up ${path}
     '';
     wgdown = pkgs.writeScriptBin "wgdown" ''
       ${createConfig}
-      sudo wg-quick down ${path}
+      sudo ${pkgs.wireguard-tools}/bin/wg-quick down ${path}
       ${deleteConfig}
     '';
   in [
